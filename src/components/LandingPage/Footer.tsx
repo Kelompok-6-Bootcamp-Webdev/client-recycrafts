96b5e1dfaa94b75bafe97bd31bc4c95fb20c3f02
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2024-08-08 17:08:25 +0700

    feat: Add Footer component to LandingPage

---
+++ src/components/LandingPage/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className="container mx-auto text-center text-white">
+        <div className="border-t-2 bg-orange-600 py-8">
+          <p>&copy; 2024, Kelompok 6 Bootcamp Website Developer SmartPath</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
