b99064591f1b6d3470e7159ba2a77a3735e86b87
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2024-08-08 17:07:56 +0700

    chore: init react

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Header from './components/LandingPage/Header'
+import Hero from './components/LandingPage/Hero'
+import Collection from './components/LandingPage/Collection'
+import Donation from './components/LandingPage/Donation'
+import Articles from './components/LandingPage/Articles'
+import Faq from './components/LandingPage/Faq'
+import Footer from './components/LandingPage/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Collection />
+      <Donation />
+      <Articles />
+      <Faq />
+      <Footer />
+    </>
+  )
+}
+
+export default App
